d194f24646501e05b37f55fb69e13698a4c6ff96
Emanuelsc1215 <142698208+Emanuelsc1215@users.noreply.github.com>
2023-10-26 16:35:20 -0500

    Version5

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let mysql=require('mysql');
 let app=express();
 app.use(express.json());
 
+
 app.listen('3000', function(){
     console.log('Servidor OK');
 })
@@ -17,7 +18,7 @@ let conexion = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'',
-    database:'articulosdb'
+    database:'articulosbd'
 })
 conexion.connect(function(error){
     if(error){
@@ -43,15 +44,15 @@ app.get('/api/articulos/:id', (req,res)=>{
             throw error;
         } else{
             res.send(fila);
-            res.send(fila[0].descripción);
+            res.send(fila[0].descripcion);
         }
     });
 });
 
 app.post('/api/articulos',(req,res)=>{
-    let data = {id:req.body.id, descripción:req.body.descripción, precio:req.body.precio, stock:req.body.stock};
+    let data = {descripcion:req.body.descripcion, precio:req.body.precio, stock:req.body.stock};
     let sql = "INSERT INTO articulos SET ?";
-    conexion.query(sql,data,function(error,result){
+    conexion.query(sql,data,function(error,results){
         if(error){
             throw error;
         } else{
